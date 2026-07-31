b33c0a3ff6797f56061d8cd3e0e0a55bf200efdf
WenHan <willy870719@gmail.com>
2026-07-31 11:35:53 +0800

    update 2026.8.3

--- Assets/EWova.Wristband/Runtimes/Scripts/PackageInfo.cs
+++ Assets/EWova.Wristband/Runtimes/Scripts/PackageInfo.cs
@@ -4,6 +4,6 @@ namespace EWova.Wristband
     internal static class PackageInfo
     {
         public const string Name = "com.ewova.wristband";
-        public const string Version = "2026.8.2";
+        public const string Version = "2026.8.3";
     }
 }
